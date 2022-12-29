5055dc15595b2476e8c29dca7fdada4fb1ccfbfd
Wise-Wizard <saransh.shankar@gmail.com>
2022-12-29 11:26:56 +0530

    Final COmmit

--- app.js
+++ app.js
@@ -3,13 +3,30 @@ const express=require("express");
 const bodyParser=require("body-parser");
 
 const app=express();
+var itemList=['Buy Food','Cook Food','Eat Food'];
 
+app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+app.use("/CSS",express.static(__dirname+"/CSS"));
+
 
 app.listen(3000,function(){
     console.log("Server is Running");
 });
 
 app.get("/",function(req,res){
-    res.send("Hello");
+
+var today=new Date();
+var options={
+    weekday:'long',
+    day:'numeric',
+    month:'long',
+   };
+var day=today.toLocaleDateString("en-US",options);
+res.render('days',{dayName:day, newItemList:itemList});
+});
+
+app.post('/',function(req,res){
+    itemList.push(req.body.TaskName);
+    res.redirect('/');
 });
