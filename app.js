936e15719623a48f5c093861a3c491cbdf6e101c
Wise-Wizard <saransh.shankar@gmail.com>
2023-01-02 23:58:15 +0530

    Added Mongo DB as Backend

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-//jshint esversion: 6
+//jshint esversion: 8
 const express=require("express");
 const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
 
 const app=express();
-var itemList=['Buy Food','Cook Food','Eat Food'];
+
 
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -23,10 +24,44 @@ var options={
     month:'long',
    };
 var day=today.toLocaleDateString("en-US",options);
-res.render('days',{dayName:day, newItemList:itemList});
+if(itemList.length===0)
+{
+    TaskList.insertMany(itemList,function(err){
+        if(err){console.log(err);}
+        else{console.log("Updated Successfully");}
+    });
+    res.redirect("/");
+}
+else{
+    res.render('days',{dayName:day, newItemList:itemList});
+}
 });
 
 app.post('/',function(req,res){
     itemList.push(req.body.TaskName);
     res.redirect('/');
 });
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/TaskManagerDB');
+}
+
+const ToDOSchema=new mongoose.Schema({
+    taskName: String,
+});
+
+const TaskList=mongoose.model("TaskList",ToDOSchema);
+const BuyFood= new TaskList({
+    taskName:"Welcome to Task Manager"
+});
+const CookFood= new TaskList({
+    taskName:"Tap the + icon to add a Task"
+});
+const EatFood= new TaskList({
+    taskName:"Tap on Checkbox to delete a Task"
+});
+
+var itemList=[BuyFood,CookFood,EatFood];
+
